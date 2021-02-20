c35bc40a5b88710d12f40b01e2afbc643a65fa18
zeeshan031995 <69483018+zeeshan031995@users.noreply.github.com>
2021-02-20 14:55:44 -0500

    commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
