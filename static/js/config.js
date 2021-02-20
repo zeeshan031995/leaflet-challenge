725fa2280827315f6aaf1b2770a79da2f7d90a03
zeeshan031995 <69483018+zeeshan031995@users.noreply.github.com>
2021-02-20 14:36:28 -0500

    commit

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiemVlc2hhbjE5OTUiLCJhIjoiY2tsZTN5YjExMGQ1czJubXU4bnUwYTZ5eCJ9.fwfN-hOUfsHiGp7NE3bxiw"
+//const API_KEY = "pk.eyJ1IjoiemVlc2hhbjE5OTUiLCJhIjoiY2tsZTNtNWc3MGQzbjJ2bXV4b2VwbnpqZiJ9.BIdpiqRLe15y4rwA8Zjh7w"
